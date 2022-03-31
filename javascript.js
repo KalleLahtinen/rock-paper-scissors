be2c8a685207aad7bacea7a508c5f163c37f29db
Kalle Lahtinen <kalle.lahtinen4@gmail.com>
2022-03-31 20:08:01 +0300

    Add functions for the players choice, a single round and the game.

--- javascript.js
+++ javascript.js
@@ -29,17 +29,79 @@ function drawHand(maxCount) {
   fucked up = ${doneFuckedUp}`)
 }
 
-function computerPlay() {
-  int = getRandomIntInclusive(1, 3)
-  let hand
+function playRound() {
+  let playerSelection = playerChoice()
+  let computerSelection = computerChoice()
 
-  if (int === 1) {
-    hand = "rock";
-  } else if (int === 2) {
-    hand = "paper";
+  if (playerSelection === computerSelection) {
+    console.log("It's a draw!");
+    return "draw"
+
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    console.log("You Lose! Paper beats rock");
+    return "computer"
+
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    console.log("You lose! Scissors beats paper");
+    return "computer"
+
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    console.log("You lose! Rock beats scissors");
+    return "computer"
+
   } else {
-    hand = "scissors";
+    console.log("You win!");
+    return "player"
   }
-  return hand
 }
 
+function computerChoice() {
+  int = getRandomIntInclusive(1, 3)
+  let choice
+
+  if (int === 1) {
+    choice = "rock";
+  } else if (int === 2) {
+    choice = "paper";
+  } else {
+    choice = "scissors";
+  }
+  return choice
+}
+
+function playerChoice() {
+  let choice
+  do {
+    choice = window.prompt("What do you choose? (Rock, Paper or Scissors)");
+    choice = choice.toLowerCase();
+  } while (["rock", "paper", "scissors"].indexOf(choice) == -1)
+
+  return choice
+}
+
+function game() {
+  rounds = parseInt(window.prompt("How many rounds do you want to play?", ""));
+  rounds = Math.abs(rounds);
+
+  let playerPoints = 0
+  let computerPoints = 0
+  
+  for (let i = 0; i < rounds; i++) {
+    let result = playRound();
+    if (result === "player") {
+      playerPoints++
+    } else if (result === "computer") {
+      computerPoints++
+    } else {
+      console.log("Something else happened")
+    }
+  }
+
+  if (playerPoints > computerPoints) {
+    window.alert(`You win! ${playerPoints}-${computerPoints}`)
+  } else if (playerPoints === computerPoints) {
+    window.alert(`It's a draw! ${playerPoints}-${computerPoints}`)
+  } else {
+    window.alert(`You lose! ${computerPoints}-${playerPoints}`)
+  }
+}
